060db95ac347afa9d35516e3c3819d46c832878c
Allie <allison.tpttn@gmail.com>
2019-09-12 14:26:13 -0500

    installed bootstrap which fixed my navbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom"
 import Wizard from './components/Wizard'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Router>
