80746c5123ec70e8deb4c66f8e06f8f16dc12310
Allie <allison.tpttn@gmail.com>
2019-09-16 15:25:01 -0500

    Steve made the async function into an arrow function and tied function to onClick

--- src/components/myspells/CompareMySpellsModal.js
+++ src/components/myspells/CompareMySpellsModal.js
@@ -2,55 +2,55 @@
 
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+const Deepai = require("deepai")
 
 class CompareMySpellsModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        };
 
-    this.toggle = this.toggle.bind(this);
-  }
+        this.toggle = this.toggle.bind(this);
+    }
 
-  toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }));
-  }
+    toggle() {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }));
+    }
 
- //   Here is where I am messing around with the Deepai external API image comparison
-    deepai = require('deepai');
-    deepai.setApiKey("quickstart-QUdJIGlzIGNvbWluZy4uLi4K"); 
+    //   Here is where I am messing around with the Deepai external API image comparison
 
     // an async function returns a promise. the values are wrapped in a resolved promise
-    async function() {
-        const resp = await deepai.callStandardApi("image-similarity", {
+    test = async () => {
+        Deepai.setApiKey("quickstart-QUdJIGlzIGNvbWluZy4uLi4K");
+        const resp = await Deepai.callStandardApi("image-similarity", {
             image1: "YOUR_IMAGE_URL", // this.state.mySpell.image ???
-            image2: "YOUR_IMAGE_URL", // this.state.spells.image ???
+            image2: "YOUR_IMAGE_URL", //  image2: fs.createReadStream("/images/.jpg"),
         });
         console.log(resp);
     };
 
 
 
-  render() {
-    return (
-      <div>
-        <Button color="warning" onClick={this.toggle}>Compare This Spell</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle} charCode="Y">Results</ModalHeader>
-          <ModalBody className="yourResultContainerId">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    render() {
+        return (
+            <div>
+                <Button color="warning" onClick={this.test}>Compare This Spell</Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle} charCode="Y">Results</ModalHeader>
+                    <ModalBody className="yourResultContainerId">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
 }
 
 export default CompareMySpellsModal;
